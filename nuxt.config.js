d0521a0e7db73e71c839431897b720e7133666cc
cjlaborde <cjlaborde@gmail.com>
2021-04-18 19:46:03 -0400

    Added head for SEO to index.vue and nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,4 +4,17 @@ export default {
         host: '0.0.0.0',
         port: 5500,
     },
+    head: {
+        // %s = placeholder for page title
+        titleTemplate: "Mastering Nuxt: %s",
+        htmlAttrs: {
+           lang: "en" 
+        },
+        bodyAttrs: {
+            class: ["my-style"]
+        },
+        meta: [{
+            charset: "utf-8",
+        }]
+    } 
 }
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,16 @@
 import homes from '~/data/homes'
 
 export default {
+   head() {
+      return {
+         title: 'Homepage',
+         meta: [{ 
+            name: 'description',
+            content: 'This is a homepage!',
+            hid: 'description'
+         }]
+      }
+   },
    data() {
       return {
          // return first 3 homes by slicing array
