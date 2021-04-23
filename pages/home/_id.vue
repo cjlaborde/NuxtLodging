8381af23543fda06f2998c01db3c59832f284686
cjlaborde <cjlaborde@gmail.com>
2021-04-23 19:28:47 -0400

    Create Host User section

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -4,7 +4,7 @@
       <img v-for="image in home.images" :key="image" :src="image" width="200" height="150"/>
     </div>      
     {{ home.title }} <br>
-    ${{ home.pricePerNight }} / night<br>
+    ${{ home.pricePerNight }} / night <br>
     <img src="/images/marker.svg" width="20" height="20"/>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{home.location.country }} <br>
     <img src="/images/star.svg" width="20" height="20"/>{{ home.reviewValue }} <br>
     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath <br>
@@ -14,8 +14,13 @@
         <img :src="review.reviewer.image"/><br/>
         {{ review.reviewer.name }}<br/>
         {{ formatDate(review.date) }}<br/>
-        <ShortText :text="review.comment" :target="150" /> <br>
+        <ShortText :text="review.comment" :target="150" /><br>
     </div>
+    <img :src="user.image"><br>
+    {{ user.name }}<br>
+    {{ formatDate(user.joined)}}<br>
+    {{ user.reviewCount }} <br>
+    {{  user.description }} <br>
 </div>
 </template>
 
@@ -46,19 +51,17 @@ export default {
 
         const reviewResponse = await $dataApi.getReviewsByHomeId(params.id);
         if (!reviewResponse.ok) return error({ statusCode: reviewResponse.status, message: reviewResponse.statusText })
+
+        const userResponse = await $dataApi.getUserByHomeId(params.id);
+        if (!userResponse.ok) return error({ statusCode: userResponse.status, message: userResponse.statusText })
+
+        console.log(userResponse.json);
+
         return {
             home: homeResponse.json,
             reviews: reviewResponse.json.hits,
+            user: userResponse.json.hits[0]
         }
     }
-    // async created() {
-        // const home = homes.find((home) => home.objectID == this.$route.params.id);
-        // console.log(this.$route);
-        // const home = await this.$dataApi.getHome(this.$route.params.id);
-        // // console.log(process.env.AlgoliaAppID);
-        // this.home = home;
-
-    // },
- 
 }
 </script>
